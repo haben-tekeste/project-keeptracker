95e72192c47aebbcecc3ba5bbb70c34f6e594e02
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-10-07 05:42:24 +0000

    Handle status

--- src/projects/projectApi.ts
+++ src/projects/projectApi.ts
@@ -1,17 +1,31 @@
 import { Project } from "./Project";
 
-const baseUrl = 'http://localhost:4000';
+const baseUrl = "http://localhost:4000";
 const url = `${baseUrl}/projects`;
 
-function translateStatusToErrorMessage(status:number){
-    switch(status){
-        case 401:
-            return "Please login again";
-        case 403:
-            return "You don't have permission to view project(s)";
-        default:
-            return 'There was an error retrieving the project(s). Please try again.';
-    }
+function translateStatusToErrorMessage(status: number) {
+  switch (status) {
+    case 401:
+      return "Please login again";
+    case 403:
+      return "You don't have permission to view project(s)";
+    default:
+      return "There was an error retrieving the project(s). Please try again.";
+  }
 }
 
+function checkStatus(response: any) {
+  if (response.ok) {
+    return response;
+  }
+  const httpErrorInfo = {
+    status: response.status,
+    statusText: response.statusText,
+    url: response.url,
+  };
 
+  console.log(`log server http error: ${JSON.stringify(httpErrorInfo)}`);
+
+  let errorMessage = translateStatusToErrorMessage(httpErrorInfo.status);
+  throw new Error(errorMessage);
+}
