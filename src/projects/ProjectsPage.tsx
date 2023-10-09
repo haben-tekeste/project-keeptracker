2a80bec9f906a044f014b6c606210799124bf652
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-10-09 05:50:25 +0000

    Fetch projects

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,17 +1,38 @@
 import { MOCK_PROJECTS } from "./MockProject";
 import ProjectList from "./ProjectList";
 import { Project } from "./Project";
-import { useState } from "react";
-import { Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
+import { projectApi } from "./projectApi";
 
 function ProjectsPage() {
-  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+
   const saveProject = (project: Project) => {
     const updatedProjects = projects.map((p: Project) => {
       return p.id === project.id ? project : p;
     });
     setProjects(updatedProjects);
   };
+
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        const data = await projectApi.get(1);
+        setError("");
+        setProjects(data);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  });
   return (
     <Fragment>
       <h1>Projects</h1>
