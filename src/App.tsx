f4143ae5ae6c3923f50a0a867dec031ab93d033f
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-10-10 06:48:33 +0000

    Add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import "./App.css";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import HomePage from "./home/HomePage";
+
 //
 import ProjectsPage from "./projects/ProjectsPage";
 
 function App() {
   return (
-    <div className="App">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
