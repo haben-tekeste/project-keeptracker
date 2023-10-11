dfe1bddd2a50f7339fa7a0d432bee68dca866c86
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-10-11 05:50:35 +0000

    Add link to project

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -1,9 +1,9 @@
 import { Project } from "./Project";
+import { Link } from "react-router-dom";
 
 type projectCardProps = {
   project: Project;
   onEdit: (project: Project) => void;
-
 };
 
 function formatDescription(description: string): string {
@@ -19,12 +19,20 @@ function ProjectCard({ project, onEdit }: projectCardProps) {
     <div className="card">
       <img src={project.imageUrl} alt={project.name} />
       <section className="section dark">
-        <h5 className="strong">
-          <strong>{project.name}</strong>
-        </h5>
-        <p>{formatDescription(project.description)}</p>
-        <p>Budget: {project.budget.toLocaleString()}</p>
-        <button className="bordered" onClick={() => handleEditClick(project)}>
+        <Link to={"/projects/" + project.id}>
+          <h5 className="strong">
+            <strong>{project.name}</strong>
+          </h5>
+          <p>{formatDescription(project.description)}</p>
+          <p>Budget : {project.budget.toLocaleString()}</p>
+        </Link>
+        <button
+          type="button"
+          className=" bordered"
+          onClick={() => {
+            handleEditClick(project);
+          }}
+        >
           <span className="icon-edit "></span>
           Edit
         </button>
