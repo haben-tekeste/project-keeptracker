205d8fe39ff76dc20064663ce587828bd38a8499
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-10-13 12:00:10 +0000

    Homepage snapshot test

--- src/home/HomePage.test.tsx
+++ src/home/HomePage.test.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import HomePage from "./HomePage";
 
-test("render home heading", () => {
-  render(<HomePage />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Home");
+describe("<HomePage/>", () => {
+  test("should render without crashing", () => {
+    render(<HomePage />);
+  });
+
+  test("render home heading", () => {
+    render(<HomePage />);
+    expect(screen.getByRole("heading")).toHaveTextContent("Home");
+  });
+
+  test("snapshot", () => {
+    const tree = renderer.create(<HomePage />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
