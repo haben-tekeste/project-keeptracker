39a8ab417a96392a60ba7ee618db17c8a390eba9
mekson bravo <95582758+haben-tekeste@users.noreply.github.com>
2023-10-16 06:22:01 +0000

    Test form displays when editing

--- src/projects/__tests__/ProjectList.test.tsx
+++ src/projects/__tests__/ProjectList.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, act } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import ProjectList from "../ProjectList";
@@ -30,5 +30,21 @@ describe("<ProjectList />", () => {
     expect(screen.getAllByRole("img")).toHaveLength(MOCK_PROJECTS.length);
     expect(screen.getAllByRole("link")).toHaveLength(MOCK_PROJECTS.length);
     expect(screen.getAllByRole("button")).toHaveLength(MOCK_PROJECTS.length);
+  });
+
+  test("Should display form when edit button is clicked", async () => {
+    setup();
+    // eslint-disable-next-line testing-library/render-result-naming-convention
+    const user = userEvent.setup();
+
+    await user.click(
+      screen.getByRole("button", { name: /edit Wisozk Group/i }),
+    );
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole("form", { name: /edit a project/i }),
+      ).toBeInTheDocument();
+    });
   });
 });
